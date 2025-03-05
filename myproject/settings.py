74fbc8dd77a89af53a5201d73de4b8d19830bb34
Ramurezaru <133072670+OneWhoUsingSwordsAsProjectileWeapon@users.noreply.github.com>
2025-03-05 15:29:43 +0900

    Реорганизация структуры проекта и реализация управления пользователями
    - Переименован файл резервной копии в ModelsBackup.py
    - Обновлены модели в приложениях cemeteries и notifications
    - Добавлены URL-конфигурации для всех приложений
    - Реализованы миграции базы данных
    - Настроена система аутентификации пользователей

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'users.User'
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,6 +41,8 @@ INSTALLED_APPS = [
     'maps',
     'notifications',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
